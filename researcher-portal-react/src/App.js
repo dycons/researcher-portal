f94114bbfc5c0f281a051b1af4f93e815d598567
Samarth Agarwal <agarwalsamarth@hotmail.com>
2021-01-25 15:51:11 -0500

    Added Katsu component.

--- researcher-portal-react/src/App.js
+++ researcher-portal-react/src/App.js
@@ -2,26 +2,28 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import LoginButton from './components/LoginButton';
+import Katsu from './components/Katsu';
 
 /*
-Returns a BrowserRouter component that displays a LoginButton component. 
-Clicking the button redirects to the Login component.
+Returns a BrowserRouter component that specifies the component
+that should be displayed for each route.
 */
 function App() {
   document.title = 'Login';
 
   return (
     <BrowserRouter>
-      <div>
-        <Switch>
-          <Route exact path='/'>
-            <LoginButton />
-          </Route>
-          <Route path='/token'>
-            <Login />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path='/'>
+          <LoginButton />
+        </Route>
+        <Route path='/home'>
+          <Login />
+        </Route>
+        <Route path='/katsu'>
+          <Katsu />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ researcher-portal-react/src/components/Katsu.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+/*
+This class is responsible for displaying Katsu clinical and phenotypic metadata.
+*/
+class Katsu extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+
+  /*
+  Fetches data from the /api/individuals endpoint of the Katsu service
+  and modifies it to only keep the id and updated fields. Sets the data
+  variable in the state to this data.
+  */
+  componentDidMount() {
+    fetch('http://localhost:8000/api/individuals')
+      .then(response => response.json())
+      .then(jsonData => jsonData.results)
+      .then(results => results.map(result => {
+        const newResult = {};
+        newResult.id = result.id;
+        newResult.updated = result.updated;
+        return newResult;
+      }))
+      .then(finalResults => this.setState({ data: finalResults }))
+      .catch(err => alert(err));
+  }
+
+  /*
+  Returns a table displaying the information in the data variable 
+  of the state.
+  */
+  render() {
+    const tableRows = this.state.data.map(data => {
+      return (
+        <tr key={data.id}>
+          <td>{data.id}</td>
+          <td>{data.updated}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Katsu;
