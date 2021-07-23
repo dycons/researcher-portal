0f25d797c87017dbac3ed7f9198433765f680376
Samarth Agarwal <agarwalsamarth@hotmail.com>
2021-07-23 17:41:06 -0400

    Created a Footer component and its CSS styling.

---
+++ researcher-portal-react/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Footer.css';
+
+/*
+Returns a component displaying the footer.
+*/
+function Footer() {
+  return (
+    <div className='footer'>
+      <span className='footer-text'>This is a demo instance of DyCons. Do not use this portal or its associated services in production.</span>
+    </div>
+  );
+}
+
+export default Footer;
